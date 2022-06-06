f8a4af5f974d100b157224ce3fda3c70860945a6
许亲风 <1161352794@qq.com>
2022-06-06 19:27:22 +0800

    增加增量数据获取方法query_latest_data

--- src/consts.rs
+++ src/consts.rs
@@ -11,6 +11,7 @@ pub const PDMS_REFNO_INFOS_TABLE: &'static str =  "REFNO_INFOS";
 pub const PDMS_DBNO_INFOS_TABLE: &'static str =  "DBNO_INFOS";
 pub const PDMS_PROJECT_MDB_TABLE: &'static str =  "PROJECT_MDB";
 pub const PDMS_DATA_STATE: &'static str = "DATA_STATE";
+pub const INCREMENT_DATA: &'static str = "INCREMENT_DATA";
 // project_mdb
 // 专业表单
 pub const PROFESSION_TX_XT:&'static str = "TX_XT";
