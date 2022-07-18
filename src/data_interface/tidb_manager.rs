0add2ea77d9b99291f71c40560960301f1919e37
dongpengcheng <golinuxlove@gmail.com>
2022-07-18 20:05:48 +0800

    update

--- src/data_interface/tidb_manager.rs
+++ src/data_interface/tidb_manager.rs
@@ -626,7 +626,7 @@ impl AiosDBManager {
         let h_ref = group_att.get_foreign_refno(if is_hang { "HREF" } else { "HSTU" }).unwrap_or_default();
         let hconnect = group_att.get_as_string("HCON").unwrap_or_default();
         let mut has_tubi = true;
-        if hconnect.as_str() == "DUCT"{
+        if hconnect.as_str() == "DUCT" || is_debug{
             has_tubi = false;
         }
         // dbg!(htube_ref);
