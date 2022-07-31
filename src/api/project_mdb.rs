a07667003e97a9fc969a72ce2734225b972b72d0
dongpengcheng <golinuxlove@gmail.com>
2022-08-01 00:24:38 +0800

    update

--- src/api/project_mdb.rs
+++ src/api/project_mdb.rs
@@ -9,6 +9,7 @@ use crate::api::element::query_mdb_module_worlds;
 
 pub async fn insert_project_mdb(pool: &Pool<MySql>, info_pool: &Pool<MySql>) -> anyhow::Result<()> {
     let project_mdb = query_mdb_module_worlds(pool, info_pool).await?;
+    let project_mdb_len = project_mdb.len();
     let sql = gen_insert_project_mdb_sql(project_mdb.clone());
     let json_sql = gen_insert_project_mdb_json_sql(project_mdb);
     if project_mdb_len != 0 {
