12cb8867cbb3803ba710ccb3e1f167a65aac5326
许亲风 <1161352794@qq.com>
2022-10-30 18:48:46 +0800

    增加 query_all_need_compute_room_refno 方法

--- src/aql_api/pdms_room.rs
+++ src/aql_api/pdms_room.rs
@@ -33,12 +33,12 @@ pub struct RoomEdgeAql {
 // pub fn save_room_info_to_arangodb(room_info:HashMap<RefU64>)
 
 /// 获取所有需要计算的房间号
-pub async fn query_all_need_compute_room_refno(dbno: Vec<i32>, room_type: &str, pool: &Pool<MySql>) -> anyhow::Result<Vec<RefU64>> {
+pub async fn query_all_need_compute_room_refno(dbno: Vec<i32>, room_type: &str, pool: &Pool<MySql>) -> anyhow::Result<Vec<(RefU64,String)>> {
     let mut refnos = vec![];
     let sql = gen_query_all_need_compute_room_refno_sql(dbno, room_type);
     let results = sqlx::query(&sql).fetch_all(&mut pool.acquire().await?).await?;
     for result in results {
-        refnos.push(RefU64(result.get::<i64,_>("ID") as u64));
+        refnos.push((RefU64(result.get::<i64, _>("ID") as u64),result.get::<String,_>("NAME")));
     }
     Ok(refnos)
 }
@@ -46,20 +46,25 @@ pub async fn query_all_need_compute_room_refno(dbno: Vec<i32>, room_type: &str, 
 fn gen_query_all_need_compute_room_refno_sql(dbnos: Vec<i32>, room_type: &str) -> String {
     let mut sql = String::new();
     let mut dbno_str = String::new();
-    for dbno in dbnos {
+    for dbno in &dbnos {
         dbno_str.push_str(&format!("{} ,", dbno.to_string()));
     }
-    dbno_str.remove(dbno_str.len() - 1);
-    sql.push_str(&format!("SELECT ID FROM {PDMS_ELEMENTS_TABLE} WHERE TYPE = '{}' AND NUMBDB IN ({})", room_type, dbno_str));
+
+    sql.push_str(&format!("SELECT ID,NAME FROM {PDMS_ELEMENTS_TABLE} WHERE TYPE = '{}'", room_type));
+    if !dbnos.is_empty() {
+        dbno_str.remove(dbno_str.len() - 1);
+        sql.push_str(&format!("AND NUMBDB IN ({})", dbno_str))
+    }
     sql
 }
 
+
 #[tokio::test]
-async fn test_query_all_need_compute_room_refno() -> anyhow::Result<()>{
+async fn test_query_all_need_compute_room_refno() -> anyhow::Result<()> {
     let _ = dotenv::dotenv();
     let url = env::var("DATABASE_URL")?;
     let pool = AiosDBManager::get_db_pool(&url, "sample").await?;
-    let results = query_all_need_compute_room_refno(vec![7200],"ROOM",&pool).await?;
+    let results = query_all_need_compute_room_refno(vec![7200], "ROOM", &pool).await?;
     dbg!(&results);
     Ok(())
 }
