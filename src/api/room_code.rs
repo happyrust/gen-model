41bce35995977d5a34eef5955a6ed2c32049169f
许亲风 <1161352794@qq.com>
2022-12-08 21:18:23 +0800

    update

--- src/api/room_code.rs
+++ src/api/room_code.rs
@@ -1,5 +1,18 @@
 use aios_core::pdms_types::RefU64;
+use arangors_lite::{AqlQuery, Database};
 use sqlx::{MySql, Pool, Row};
+use crate::aql_api::convert_refno_vec_from_vec_string;
+
+/// 查找房间下的所有元件的参考号
+pub async fn query_room_refnos_aql(refno: RefU64, database: &Database) -> anyhow::Result<Vec<RefU64>> {
+    let key = format!("room_eles/{}", refno.to_url_refno());
+    let aql = AqlQuery::new("
+        for v,e in 1 outbound @key room_edges
+            return v._key
+        ").bind_var("key", key);
+    let result: Vec<String> = database.aql_query(aql).await?;
+    Ok(convert_refno_vec_from_vec_string(result))
+}
 
 /// 查询房间号
 pub async fn query_room_code(refno: RefU64, pool: &Pool<MySql>) -> anyhow::Result<Option<String>> {
