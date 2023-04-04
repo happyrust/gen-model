3097aa23083e41a72073d213579f0999ef9fa5e3
dongpengcheng <golinuxlove@gmail.com>
2023-04-04 14:26:18 +0800

    fix dish problem

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,42 @@ extern crate clap;
 #[macro_use]
 extern crate nom;
 
-use std::collections::{BTreeMap, HashMap, HashSet};
-use std::fmt::format;
-use std::fs;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::mem::take;
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
-use std::time::{Instant, UNIX_EPOCH};
 use aios_core::accel_tree::acceleration_tree::{AccelerationTree, RStarBoundingBox};
 use aios_core::db_number::DbNumMgr;
-use aios_core::pdms_types::*;
 use aios_core::pdms_types::AttrVal::StringType;
+use aios_core::pdms_types::*;
 use aios_core::prim_geo;
 use aios_core::tool::db_tool::{db1_dehash, db1_hash, read_attr_info_config_from_bin};
+use aios_database::api::admin::query_all_db_infos;
+use aios_database::api::attr::insert_attr_info;
+use aios_database::api::element::*;
+use aios_database::api::ssc_data::{get_ancestor_till_type, query_all_room_data, update_ssc_type};
+use aios_database::aql_api::foreign_refnos::query_foreign_name_aql;
+use aios_database::aql_api::pdms_room::{
+    query_all_need_compute_room_refno, save_room_info_to_arangodb, RoomEdgeAql, RoomElementAql,
+};
+use aios_database::aql_api::tubi::{insert_tubi_value, query_all_tubi_from_node};
+use aios_database::cata::resolve::parse_to_i32;
+use aios_database::consts::*;
+use aios_database::data_interface::interface::PdmsDataInterface;
+use aios_database::data_interface::tidb_manager::AiosDBManager;
+use aios_database::database::*;
+use aios_database::graph_db::pdms_arango::*;
+use aios_database::graph_db::pdms_inst_arango::{
+    query_instance_with_refno_in_arangodb, sync_instance_to_graph_db,
+};
+use aios_database::graph_db::pdms_mesh_arango::sync_mesh_to_graph_db;
+use aios_database::graph_db::ssc_arango::set_arangodb_all_ssc_nodes;
+use aios_database::helper::{qualified_column_name, qualified_table_name};
+use aios_database::negative::{compute_boolean_mesh, query_negative_refnos_aql};
+use aios_database::options::DbOption;
+use aios_database::spatial_tree::recompute_spatial_tree;
+use aios_database::ssc::{async_total_ssc_data, get_rooms_from_excel};
+use aios_database::tables::*;
+use aios_database::BATCH_CHUNKS_CNT;
 use arangors_lite::collection::CollectionType::{Document, Edge};
+use bevy::prelude::*;
+use bevy::transform::components::Transform;
 use chrono::{Datelike, Timelike};
 use dashmap::DashMap;
 use futures::StreamExt;
@@ -36,38 +56,21 @@ use parry3d::shape::{Compound, ConvexPolyhedron, SharedShape};
 use parry3d::transformation::vhacd;
 use parry3d::transformation::vhacd::VHACD;
 use parse_pdms_db::parse::{PdmsDbData, WholeAttMap};
-use regex::internal::Input;
-use sqlx::{Acquire, MySql, MySqlPool, Pool, Row};
-use sqlx::Executor;
-use sqlx::pool::PoolConnection;
-use aios_database::api::attr::insert_attr_info;
-use aios_database::api::element::*;
-use aios_database::api::ssc_data::{get_ancestor_till_type, query_all_room_data, update_ssc_type};
-use aios_database::aql_api::foreign_refnos::query_foreign_name_aql;
-use aios_database::BATCH_CHUNKS_CNT;
-use aios_database::aql_api::pdms_room::{query_all_need_compute_room_refno, RoomEdgeAql, RoomElementAql, save_room_info_to_arangodb};
-use aios_database::cata::resolve::parse_to_i32;
-use aios_database::consts::*;
-use aios_database::data_interface::interface::PdmsDataInterface;
-use aios_database::data_interface::tidb_manager::AiosDBManager;
-use aios_database::database::*;
-use aios_database::graph_db::pdms_arango::*;
-use aios_database::graph_db::pdms_inst_arango::{query_instance_with_refno_in_arangodb, sync_instance_to_graph_db};
-use aios_database::graph_db::pdms_mesh_arango::sync_mesh_to_graph_db;
-use aios_database::graph_db::ssc_arango::set_arangodb_all_ssc_nodes;
-use aios_database::helper::{qualified_column_name, qualified_table_name};
-use aios_database::options::DbOption;
-use aios_database::ssc::{async_total_ssc_data, get_rooms_from_excel};
-use aios_database::tables::*;
-use bevy::prelude::*;
-use bevy::transform::components::Transform;
 use parse_pdms_db::parse_file;
+use regex::internal::Input;
+use sqlx::pool::PoolConnection;
+use sqlx::Executor;
+use sqlx::{Acquire, MySql, MySqlPool, Pool, Row};
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::fmt::format;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::mem::take;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex};
+use std::time::{Instant, UNIX_EPOCH};
 use tokio::spawn;
-use aios_database::api::admin::query_all_db_infos;
-use aios_database::aql_api::tubi::{insert_tubi_value, query_all_tubi_from_node};
-use aios_database::negative::{compute_boolean_mesh, query_negative_refnos_aql};
-use aios_database::spatial_tree::recompute_spatial_tree;
-
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -78,7 +81,9 @@ async fn main() -> anyhow::Result<()> {
     let db_option: DbOption = s.try_deserialize().unwrap();
     /// 是否全部同步模型
     if db_option.total_sync {
-        create_arangodb_conns(&db_option).await.expect("Failed to create arangodb conns");
+        create_arangodb_conns(&db_option)
+            .await
+            .expect("Failed to create arangodb conns");
         // 把pdms数据同步到mysql
         sync_pdms(&db_option).await.unwrap();
     }
@@ -114,7 +119,9 @@ async fn main() -> anyhow::Result<()> {
         for path in children_files {
             let path = path?.path();
             let filename = path.file_name().unwrap().to_str().unwrap();
-            if !filename.ends_with("inst") { continue; }
+            if !filename.ends_with("inst") {
+                continue;
+            }
             let dbno: u32 = path.file_stem().unwrap().to_str().unwrap().parse().unwrap();
             let mut file = fs::File::open(path)?;
             let mut data = vec![];
@@ -144,7 +151,9 @@ async fn main() -> anyhow::Result<()> {
                 for path in children_files {
                     let path = path?.path();
                     let filename = path.file_name().unwrap().to_str().unwrap().to_string();
-                    if !filename.ends_with("bin") { continue; }
+                    if !filename.ends_with("bin") {
+                        continue;
+                    }
                     let mut file = fs::File::open(path)?;
                     let mut data = vec![];
                     file.read_to_end(&mut data)?;
@@ -176,10 +185,14 @@ async fn main() -> anyhow::Result<()> {
             let path = path?.path();
             dbg!(&path);
             let filename = path.file_name().unwrap().to_str().unwrap().to_string();
-            if !filename.contains("inst") { continue; }
+            if !filename.contains("inst") {
+                continue;
+            }
             let dbno_str = filename.split('.').collect::<Vec<_>>();
             let dbno = dbno_str.first().unwrap_or(&"");
-            if arch_db_nums.contains(&dbno.parse().unwrap_or(0)) { continue; }
+            if arch_db_nums.contains(&dbno.parse().unwrap_or(0)) {
+                continue;
+            }
 
             let mut file = fs::File::open(path)?;
             let mut data = vec![];
@@ -190,7 +203,8 @@ async fn main() -> anyhow::Result<()> {
                     if let Some(aabb) = kv.value().aabb {
                         if aabb.extents().magnitude().is_finite() {
                             rstar_objs.push(RStarBoundingBox::from_aabb(&aabb, *kv.key()));
-                        } else {}
+                        } else {
+                        }
                     }
                 }
             }
@@ -207,9 +221,17 @@ async fn main() -> anyhow::Result<()> {
     if db_option.save_spatial_tree_to_db {
         let mut site_major_map = HashMap::new();
         // let room_infos = vec![RefU64::from_two_nums(24381,34919)];
-        let room_infos = query_all_need_compute_room_refno(&vec![7200], "LOOP", Some("LOOP 1"), &mgr.project_map.get(&db_option.project_name).unwrap()).await?;
+        let room_infos = query_all_need_compute_room_refno(
+            &vec![7200],
+            "LOOP",
+            Some("LOOP 1"),
+            &mgr.project_map.get(&db_option.project_name).unwrap(),
+        )
+        .await?;
+        // dbg!(&room_infos);
         let room_infos = room_infos.into_iter().map(|x| x.0).collect::<Vec<_>>();
-        let map = recompute_spatial_tree(room_infos, all_insts_mgr, collider_shape_mgr, &db_option).await?;
+        let map = recompute_spatial_tree(room_infos, all_insts_mgr, collider_shape_mgr, &db_option)
+            .await?;
         save_room_info_to_arangodb(&mgr, map, &db_option, &mut site_major_map).await?;
     }
 
@@ -226,7 +248,9 @@ async fn main() -> anyhow::Result<()> {
                 let mut tubi_map_clone = tubi_map.clone();
                 let handle = tokio::spawn(async move {
                     // let database = get_arangodb_conn_from_db_option(&db_option_clone).await.unwrap();
-                    query_all_tubi_from_node(bran, &mut tubi_map_clone, &database, &pool_clone).await.unwrap_or_default();
+                    query_all_tubi_from_node(bran, &mut tubi_map_clone, &database, &pool_clone)
+                        .await
+                        .unwrap_or_default();
                 });
                 handles.push(handle);
             }
@@ -296,7 +320,6 @@ async fn create_arangodb_conns(db_option: &DbOption) -> anyhow::Result<()> {
     Ok(())
 }
 
-
 #[test]
 fn get_noun_hash() {
     let noun = "USER";
@@ -310,8 +333,16 @@ fn get_noun_hash() {
 fn test_time() {
     use chrono::prelude::*;
     let local: DateTime<Local> = Local::now();
-    println!("year:{} , month: {} , day: {}, week_day:{},hour:{} , min: {} , sec:{}", local.year(), local.month(), local.day(), local.weekday(),
-             local.hour(), local.minute(), local.second());
+    println!(
+        "year:{} , month: {} , day: {}, week_day:{},hour:{} , min: {} , sec:{}",
+        local.year(),
+        local.month(),
+        local.day(),
+        local.weekday(),
+        local.hour(),
+        local.minute(),
+        local.second()
+    );
 }
 
 /// 将 all_attr_info.bin 文件转成 json
