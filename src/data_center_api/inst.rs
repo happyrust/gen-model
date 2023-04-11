c277731227af79c5372e594f21aeb61d47d618ce
许亲风 <1161352794@qq.com>
2023-04-11 11:42:49 +0800

    增加数据中台阀门接口

--- src/data_center_api/inst.rs
+++ src/data_center_api/inst.rs
@@ -7,18 +7,21 @@ use crate::aql_api::children::{query_travel_children_aql, query_travel_children_
 use crate::graph_db::pdms_arango::get_arangodb_conn_from_db_option;
 use crate::options::DbOption;
 
-pub async fn get_valv_data(refno: RefU64, database: &Database) -> DataCenterProject {
+pub async fn get_inst_data(refno: RefU64, database: &Database) -> DataCenterProject {
     let mut instance = Vec::new();
     if let Ok(valves) = query_travel_children_with_type_aql(database, refno, "INST").await {
         for valv in valves {
             instance.push(DataCenterInstance {
-                object_model_code: "COMPBA".to_string(),
+                object_model_code: "COMPADD".to_string(),
                 project_code: "1516".to_string(),
                 instance_code: if valv.name.starts_with("/") { valv.name[1..].to_string() } else { valv.name },
                 version: "A版".to_string(),
                 attributes: vec![DataCenterAttr {
-                    attribute_model_code: "COMP8".to_string(),
+                    attribute_model_code: "COMPAD".to_string(),
                     value: "1R101".to_string(),
+                },DataCenterAttr{
+                    attribute_model_code: "COMPADD47".to_string(),
+                    value: "1000.0".to_string(),
                 }],
             });
         }
@@ -40,7 +43,7 @@ async fn test_valv() -> anyhow::Result<()> {
     let db_option: DbOption = s.try_deserialize().unwrap();
     let database = get_arangodb_conn_from_db_option(&db_option).await?;
     let refno = RefU64::from_refno_str("24381/104050").unwrap();
-    let data = get_valv_data(refno, &database).await;
+    let data = get_inst_data(refno, &database).await;
     let mut file = fs::File::create("仪控.json")?;
     let data = serde_json::to_string(&data).unwrap();
     file.write_all(&data.into_bytes())?;
