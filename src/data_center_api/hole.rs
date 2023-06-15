d3839965e89c0c34cb2e13dc84334264de66ab6e
许亲风 <1161352794@qq.com>
2023-06-15 14:21:55 +0800

    修改孔洞埋件接口

--- src/data_center_api/hole.rs
+++ src/data_center_api/hole.rs
@@ -877,50 +877,7 @@ pub async fn query_hole_data_by_keys_aql(keys: Vec<String>, database: &ArDatabas
     for key in @keys
         let c = document(@@hole_collection,key)
         filter c != null
-        return {
-            '_key': c._key,
-            'RelyItem': c.RelyItem,
-            'MainItem': c.MainItem,
-            'Speciality': c.Speciality,
-            'Position': c.Position,
-            'HoleWork': c.HoleWork,
-            'WorkBy': c.WorkBy,
-            'Time': c.Time,
-            'Shape': c.Shape,
-            'Ori': c.Ori,
-            'ItemREF': c.ItemREF,
-            'RelyItemREF': c.RelyItemREF,
-            'MainItemREF': c.MainItemREF,
-            'OpenItem': c.OpenItem,
-            'PlugType': c.PlugType,
-            'SizeHeight': c.SizeHeight,
-            'SizeWidth': c.SizeWidth,
-            'BankWidth': c.BankWidth,
-            'BankHeight': c.BankHeight,
-            'HotDis': c.HotDis,
-            'HeatThick': c.HeatThick,
-            'refNo': c.refNo,
-            'FittRefNo': c.FittRefNo,
-            'SubsMaterial': c.SubsMaterial,
-            'SubsThickness': c.SubsThickness,
-            'iCreate': c.iCreate,
-            'SubsType': c.SubsType,
-            'ExtentLength1': c.ExtentLength1,
-            'ExtentLength2': c.ExtentLength2,
-            'Second': c.Second,
-            'ReHole': c.ReHole,
-            'Note': c.Note,
-            'SizeThrowWall': c.SizeThrowWall,
-            'HoleBPID': c.HoleBPID,
-            'HoleBPVER': c.HoleBPVER,
-            'RelyItemBPID': c.RelyItemBPID,
-            'RelyItemBPVER': c.RelyItemBPVER,
-            'MainPipeline': c.MainPipeline,
-            'iFlowState': c.iFlowState,
-            'hType': c.hType,
-            'MainItems': c.MainItems,
-            'MainItemRefs': c.MainItemRefs
-        }")
+        return unset(c , '_id','_rev')")
         .bind_var("keys", keys)
         .bind_var("@hole_collection", AQL_HOLE_DATA_COLLECTION).build();
     let result = database.aql_query::<VirtualHoleGraphNode>(aql).await?;
@@ -931,50 +888,7 @@ pub async fn query_hole_data_by_keys_aql(keys: Vec<String>, database: &ArDatabas
 pub async fn query_hole_data_total_aql(database: &ArDatabase) -> anyhow::Result<Vec<VirtualHoleGraphNode>> {
     let aql = AqlQuery::builder().query("
     for c in @@collection
-        return {
-            '_key': c._key,
-            'RelyItem': c.RelyItem,
-            'MainItem': c.MainItem,
-            'Speciality': c.Speciality,
-            'Position': c.Position,
-            'HoleWork': c.HoleWork,
-            'WorkBy': c.WorkBy,
-            'Time': c.Time,
-            'Shape': c.Shape,
-            'Ori': c.Ori,
-            'ItemREF': c.ItemREF,
-            'RelyItemREF': c.RelyItemREF,
-            'MainItemREF': c.MainItemREF,
-            'OpenItem': c.OpenItem,
-            'PlugType': c.PlugType,
-            'SizeHeight': c.SizeHeight,
-            'SizeWidth': c.SizeWidth,
-            'BankWidth': c.BankWidth,
-            'BankHeight': c.BankHeight,
-            'HotDis': c.HotDis,
-            'HeatThick': c.HeatThick,
-            'refNo': c.refNo,
-            'FittRefNo': c.FittRefNo,
-            'SubsMaterial': c.SubsMaterial,
-            'SubsThickness': c.SubsThickness,
-            'iCreate': c.iCreate,
-            'SubsType': c.SubsType,
-            'ExtentLength1': c.ExtentLength1,
-            'ExtentLength2': c.ExtentLength2,
-            'Second': c.Second,
-            'ReHole': c.ReHole,
-            'Note': c.Note,
-            'SizeThrowWall': c.SizeThrowWall,
-            'HoleBPID': c.HoleBPID,
-            'HoleBPVER': c.HoleBPVER,
-            'RelyItemBPID': c.RelyItemBPID,
-            'RelyItemBPVER': c.RelyItemBPVER,
-            'MainPipeline': c.MainPipeline,
-            'iFlowState': c.iFlowState,
-            'hType': c.hType,
-            'MainItems': c.MainItems,
-            'MainItemRefs': c.MainItemRefs
-        }").bind_var("@collection", AQL_HOLE_DATA_COLLECTION).build();
+        return unset(c , '_id','_rev')").bind_var("@collection", AQL_HOLE_DATA_COLLECTION).build();
     let result = database.aql_query::<VirtualHoleGraphNode>(aql).await?;
     Ok(result)
 }
