21b6515f53c6e9886161f8b979c8f47f5c5b2adc
dpc <happydpc@sina.com>
2023-08-02 13:32:18 +0800

    水工计算test

--- src/plug_in/water_calculation.rs
+++ src/plug_in/water_calculation.rs
@@ -1,13 +1,20 @@
+use std::collections::hash_map::DefaultHasher;
 use aios_core::pdms_types::RefU64;
 use crate::data_interface::tidb_manager::AiosDBManager;
 use std::collections::HashSet;
 use aios_core::pdms_types::AttrMap;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use crate::api::children::travel_children_with_type;
 use crate::data_interface::interface::PdmsDataInterface;
 use aios_core::water_calculation::{CivilEngineeringStp, FloodingHole, FloodingHoleVec, WaterComputeStp};
 use aios_core::water_calculation::ExportFloodingStpEvent;
+use arangors_lite::AqlQuery;
 use crate::api::attr::query_attr;
+use crate::consts::AQL_WATER_CALCULATION_COLLECTION;
+use crate::graph_db::pdms_arango::{ArDatabase, save_arangodb_doc};
+use aios_core::water_calculation::FloodingStpToArangodb;
+
 
 ///得到导出stp的选中节点的参考号
 pub async fn get_hole_refno(aios_mgr: &AiosDBManager, types: &HashSet<&str>, water_compute: &mut WaterComputeStp, flooding_hole_vec: &mut FloodingHoleVec, i: &(RefU64, String)) {
@@ -31,7 +38,7 @@ pub async fn get_hole_refno(aios_mgr: &AiosDBManager, types: &HashSet<&str>, wat
 }
 
 ///得到导出stp所需孔洞数据
-pub async fn get_detail_data_for_export_stp(aios_mgr: &AiosDBManager, mut data: ExportFloodingStpEvent) -> WaterComputeStp {
+pub async fn get_detail_data_for_export_stp(aios_mgr: &AiosDBManager, mut data: ExportFloodingStpEvent) -> ExportFloodingStpEvent {
     //向上找到对应的wall
     let att_type = HashSet::from(["CWALL", "STWALL", "GWALL", "WALL", "CFLOOR", "FLOOR"]);
     for i in &data.refnos {
@@ -67,11 +74,47 @@ pub async fn get_detail_data_for_export_stp(aios_mgr: &AiosDBManager, mut data: 
             refno = basic.get_owner();
         }
     }
-    data.stp
+    data
+}
+
+pub async fn save_stp_data_to_arangodb(aios_mgr: &AiosDBManager, mut stp: ExportFloodingStpEvent) -> String {
+    // 将数据保存至图数据库
+    if let Ok(database) = aios_mgr.get_arango_db().await {
+        let mut hasher = DefaultHasher::new();
+        stp.file_name.hash(&mut hasher);
+        let key = hasher.finish();
+        let json_data = vec![stp.to_arango_struct()];
+        let Ok(send_value) = serde_json::to_value(&json_data) else { return "数据结构反序列化失败".to_string(); };
+        if let Ok(_result) = query_water_calculation_data(&database, key.to_string()).await {
+            let _ = save_arangodb_doc(send_value, AQL_WATER_CALCULATION_COLLECTION, &database, true).await.unwrap();
+        } else {
+            let _ = save_arangodb_doc(send_value, AQL_WATER_CALCULATION_COLLECTION, &database, false).await.unwrap();
+        }
+    }
+    "Ok".to_string()
 }
 
 ///导出水淹计算stp
 pub fn export_stp(data: WaterComputeStp) -> WaterComputeStp {
-    dbg!(&data);
+    // dbg!(&data);
     data
 }
+
+
+pub async fn query_water_calculation_data(database: &ArDatabase, key_value: String) -> anyhow::Result<Option<Vec<FloodingStpToArangodb>>> {
+    let aql = AqlQuery::new("let v = document('water_calculaion',@_key)\
+        return unset(v , '_id','_rev') ")
+        .bind_var("_key", key_value);
+    let data_vec: Vec<FloodingStpToArangodb> = database.aql_query(aql).await?;
+    return Ok(Some((data_vec)));
+}
+
+pub async fn query_water_calculation_data_total_aql(database: &ArDatabase) -> anyhow::Result<Vec<FloodingStpToArangodb>> {
+    let aql = AqlQuery::new("
+    for c in @@collection
+        return unset(c , '_id','_rev')").bind_var("@collection", AQL_WATER_CALCULATION_COLLECTION);
+    let result = database.aql_query::<FloodingStpToArangodb>(aql).await?;
+    Ok(result)
+}
+
+
--- src/test/test_api/test_water_calculation_stp.rs
+++ src/test/test_api/test_water_calculation_stp.rs
@@ -8,6 +8,7 @@ use crate::plug_in::water_calculation::export_stp;
 
 #[test]
 fn test_export_water_calculation_stp() {
+    //测试样例1(含孔洞，门洞，以及非土建模型)
     let mut stp = WaterComputeStp::default();
     let mut map_1 = HashMap::new();
     map_1.insert(RefU64::from_str("17496/107396").unwrap(), vec![CivilEngineeringStp {
@@ -33,6 +34,58 @@ fn test_export_water_calculation_stp() {
     ]);
     stp.civil_engineering = vec![map_1, map_2];
     stp.non_civil_engineering.push(RefU64::from_str("24381/34110").unwrap());
-    // dbg!(&stp);
+
+    //测试样例2(孔洞类型)
+    let mut stp = WaterComputeStp::default();
+    let mut map_1 = HashMap::new();
+    map_1.insert(RefU64::from_str("17496/106430").unwrap(), vec![CivilEngineeringStp {
+        wall_refno: RefU64::from_str(" 17496/106430").unwrap(),
+        hole_refno: Some(RefU64::from_str("17496/145221").unwrap()),
+        door_refno: None,
+    }, CivilEngineeringStp {
+        wall_refno: RefU64::from_str("17496/106430").unwrap(),
+        hole_refno: Some(RefU64::from_str("17496/145333").unwrap()),
+        door_refno: None,
+    }, CivilEngineeringStp {
+        wall_refno: RefU64::from_str("17496/106430").unwrap(),
+        hole_refno: Some(RefU64::from_str("17496/145334").unwrap()),
+        door_refno: None,
+    }, CivilEngineeringStp {
+        wall_refno: RefU64::from_str("17496/106430").unwrap(),
+        hole_refno: Some(RefU64::from_str("17496/157058").unwrap()),
+        door_refno: None,
+    },
+    ]);
+    stp.civil_engineering = vec![map_1];
+
+
+    // //测试样例3(门洞测试)
+    // let mut stp = WaterComputeStp::default();
+    // let mut map_1 = HashMap::new();
+    // map_1.insert(RefU64::from_str("17496/107397").unwrap(), vec![CivilEngineeringStp {
+    //     wall_refno: RefU64::from_str("17496/107397").unwrap(),
+    //     hole_refno: None,
+    //     door_refno: Some(RefU64::from_str("17496/124123").unwrap()),
+    // }, CivilEngineeringStp {
+    //     wall_refno: RefU64::from_str("17496/107397").unwrap(),
+    //     hole_refno: None,
+    //     door_refno: Some(RefU64::from_str("17496/124124").unwrap()),
+    // },
+    // ]);
+    // let mut map_2 = HashMap::new();
+    // map_2.insert(RefU64::from_str("17496/106640").unwrap(), vec![CivilEngineeringStp {
+    //     wall_refno: RefU64::from_str("17496/107037").unwrap(),
+    //     hole_refno: Some(RefU64::from_str("17496/107067").unwrap()),
+    //     door_refno: None,
+    // }, CivilEngineeringStp {
+    //     wall_refno: RefU64::from_str("17496/107397").unwrap(),
+    //     hole_refno: Some(RefU64::from_str("17496/107068").unwrap()),
+    //     door_refno: None,
+    // },
+    // ]);
+    // stp.civil_engineering = vec![map_1, map_2];
+    // stp.non_civil_engineering.push(RefU64::from_str("24381/34110").unwrap());
+
+
     export_stp(stp);
 }
