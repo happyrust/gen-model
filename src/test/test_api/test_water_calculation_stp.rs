10a3d007c5ea452ae969e55f50e7f5b4d7c84311
dongpengcheng <golinuxlove@gmail.com>
2023-08-04 11:07:35 +0800

    refactor

--- src/test/test_api/test_water_calculation_stp.rs
+++ src/test/test_api/test_water_calculation_stp.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 use aios_core::pdms_types::RefU64;
-use aios_core::water_calculation::{CivilEngineeringStp, WaterComputeStp};
+use aios_core::water_calculation::{CivilEngineeringStp, WaterComputeStpInput};
 #[cfg(feature = "opencascade_rs")]
 use opencascade::primitives::Compound;
 use sqlx::encode::IsNull::No;
@@ -13,25 +13,16 @@ use crate::test::test_helper::get_test_ams_db_manager_async;
 #[cfg(feature = "opencascade_rs")]
 #[tokio::test]
 async fn test_export_water_calculation_stp()  -> anyhow::Result<()> {
-    let mut stp_packet = WaterComputeStp::default();
+    let mut stp_packet = WaterComputeStpInput::default();
     let mut map_1 = HashMap::new();
-    map_1.insert(RefU64::from_str("17496/106429").unwrap(), vec![CivilEngineeringStp {
+    map_1.insert(RefU64::from_str("17496/106430").unwrap(), vec![ CivilEngineeringStp {
         wall_refno: RefU64::from_str("17496/106430").unwrap(),
-        hole_refno: Some(RefU64::from_str("17496/145221").unwrap()),
-        door_refno: None,
-    }, CivilEngineeringStp {
-        wall_refno: RefU64::from_str("17496/106430").unwrap(),
-        hole_refno: Some(RefU64::from_str("17496/145333").unwrap()),
-        door_refno: None,
-    }, CivilEngineeringStp {
-        wall_refno: RefU64::from_str("17496/106430").unwrap(),
-        hole_refno: Some(RefU64::from_str("17496/145334").unwrap()),
-        door_refno: None,
-    }, CivilEngineeringStp {
-        wall_refno: RefU64::from_str("17496/106430").unwrap(),
-        hole_refno: Some(RefU64::from_str("17496/157058").unwrap()),
-        door_refno: None,
-    }, ]);
+        hole_refnos: vec!(RefU64::from_str("17496/145333").unwrap(),
+                          RefU64::from_str("17496/145334").unwrap(),
+                          RefU64::from_str("17496/157058").unwrap()
+        ),
+        door_refnos: vec![],
+    }]);
     stp_packet.civil_engineering = vec![map_1];
 
     // //测试样例2(门洞模型测试)
@@ -49,6 +40,6 @@ async fn test_export_water_calculation_stp()  -> anyhow::Result<()> {
     // stp.civil_engineering = vec![map_1];
 
     let mgr = get_test_ams_db_manager_async().await;
-    export_stp(&mgr, &stp_packet).await?;
+    export_stp(&mgr, &stp_packet, "test_export").await?;
     Ok(())
 }
