0efcba445b5ded79795654593bbeaa7209bd0668
dpc <happydpc@sina.com>
2023-08-08 10:35:00 +0800

    增加测试案例

--- src/test/test_api/test_water_calculation_stp.rs
+++ src/test/test_api/test_water_calculation_stp.rs
@@ -15,45 +15,45 @@ use crate::test::test_helper::get_test_ams_db_manager_async;
 #[tokio::test]
 async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 // //测试样例1(孔洞模型测试)
-    let mut stp_packet_vec = ExportFloodingStpEvent::default();
-    stp_packet_vec.file_name = "孔洞测试1".to_string();
-    stp_packet_vec.save_time = "2023-08-07 20:39:16.867354400 +08:00".to_string();
-    stp_packet_vec.stp = vec![CivilEngineeringStp {
-        wall_refno: RefU64::from_str("17496/106430").unwrap(),
-        hole_refnos: vec![RefU64::from_str("17496/145333").unwrap(), RefU64::from_str("17496/157058").unwrap()],
-        door_refnos: vec![],
-    }];
-    stp_packet_vec.model_list = vec![(RefU64::from_str("17496/106430").unwrap(), "STWALL 1".to_string())];
-
-    let mut all_hole_map = HashMap::default();
-    let map_value = vec![FloodingHole {
-        refno: RefU64::from_str("17496/145221").unwrap(),
-        name: "/1RS05TT0016T".to_string(),
-        is_door: false,
-        is_selected: false,
-    }, FloodingHole {
-        refno: RefU64::from_str("17496/145334").unwrap(),
-        name: "/1RS05LL0027T".to_string(),
-        is_door: false,
-        is_selected: false,
-    }];
-    all_hole_map.insert(RefU64::from_str("17496/106430").unwrap(), map_value);
-    stp_packet_vec.all_hole_list = vec![all_hole_map];
-
-    let mut selected_hole_map = HashMap::default();
-    let map_value = vec![FloodingHole {
-        refno: RefU64::from_str("17496/145333").unwrap(),
-        name: "/1RS05LL0028T".to_string(),
-        is_door: false,
-        is_selected: false,
-    }, FloodingHole {
-        refno: RefU64::from_str("17496/157058").unwrap(),
-        name: "/1RS06PP0001K".to_string(),
-        is_door: false,
-        is_selected: false,
-    }];
-    selected_hole_map.insert(RefU64::from_str("17496/106430").unwrap(), map_value);
-    stp_packet_vec.selected_hole_list = vec![selected_hole_map];
+//     let mut stp_packet_vec = ExportFloodingStpEvent::default();
+//     stp_packet_vec.file_name = "孔洞测试1".to_string();
+//     stp_packet_vec.save_time = "2023-08-07 20:39:16.867354400 +08:00".to_string();
+//     stp_packet_vec.stp = vec![CivilEngineeringStp {
+//         wall_refno: RefU64::from_str("17496/106430").unwrap(),
+//         hole_refnos: vec![RefU64::from_str("17496/145333").unwrap(), RefU64::from_str("17496/157058").unwrap()],
+//         door_refnos: vec![],
+//     }];
+//     stp_packet_vec.model_list = vec![(RefU64::from_str("17496/106430").unwrap(), "STWALL 1".to_string())];
+//
+//     let mut all_hole_map = HashMap::default();
+//     let map_value = vec![FloodingHole {
+//         refno: RefU64::from_str("17496/145221").unwrap(),
+//         name: "/1RS05TT0016T".to_string(),
+//         is_door: false,
+//         is_selected: false,
+//     }, FloodingHole {
+//         refno: RefU64::from_str("17496/145334").unwrap(),
+//         name: "/1RS05LL0027T".to_string(),
+//         is_door: false,
+//         is_selected: false,
+//     }];
+//     all_hole_map.insert(RefU64::from_str("17496/106430").unwrap(), map_value);
+//     stp_packet_vec.all_hole_list = vec![all_hole_map];
+//
+//     let mut selected_hole_map = HashMap::default();
+//     let map_value = vec![FloodingHole {
+//         refno: RefU64::from_str("17496/145333").unwrap(),
+//         name: "/1RS05LL0028T".to_string(),
+//         is_door: false,
+//         is_selected: false,
+//     }, FloodingHole {
+//         refno: RefU64::from_str("17496/157058").unwrap(),
+//         name: "/1RS06PP0001K".to_string(),
+//         is_door: false,
+//         is_selected: false,
+//     }];
+//     selected_hole_map.insert(RefU64::from_str("17496/106430").unwrap(), map_value);
+//     stp_packet_vec.selected_hole_list = vec![selected_hole_map];
 
 
 // //测试样例2(孔洞模型测试)
@@ -67,7 +67,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     }];
 //     stp_packet_vec.model_list = vec![(RefU64::from_str("17496/106028").unwrap(), "STWALL 1".to_string())];
 //
-//     let all_hole_map = HashMap::default();
+//     let mut all_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("17496/106029").unwrap(),
 //         name: "/Copy-of-R445-M01".to_string(),
@@ -87,7 +87,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     all_hole_map.insert(RefU64::from_str("17496/106028").unwrap(), map_value);
 //     stp_packet_vec.all_hole_list = vec![all_hole_map];
 //
-//     let selected_hole_map = HashMap::default();
+//     let mut selected_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("17496/142305").unwrap(),
 //         name: "/1RS04CC2302T".to_string(),
@@ -114,7 +114,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     }];
 //     stp_packet_vec.model_list = vec![(RefU64::from_str("25688/8143").unwrap(), "STWALL 1".to_string())];
 //
-//     let all_hole_map = HashMap::default();
+//     let mut all_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/8144").unwrap(),
 //         name:  "/1AR01WW0002K".to_string(),
@@ -124,7 +124,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     all_hole_map.insert(RefU64::from_str("25688/8143").unwrap(), map_value);
 //     stp_packet_vec.all_hole_list = vec![all_hole_map];
 //
-//     let selected_hole_map = HashMap::default();
+//     let mut selected_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/8186").unwrap(),
 //         name: "FITT 41".to_string(),
@@ -151,7 +151,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     }];
 //     stp_packet_vec.model_list = vec![(RefU64::from_str("25688/19684").unwrap(), "STWALL 3".to_string())];
 //
-//     let all_hole_map = HashMap::default();
+//     let mut all_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/19685").unwrap(),
 //         name: "/1AR04VV0005K".to_string(),
@@ -167,7 +167,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     all_hole_map.insert(RefU64::from_str("25688/19684").unwrap(), map_value);
 //     stp_packet_vec.all_hole_list = vec![all_hole_map];
 //
-//     let selected_hole_map = HashMap::default();
+//     let mut selected_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/19702").unwrap(),
 //         name: "FITT 18".to_string(),
@@ -198,7 +198,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     }];
 //     stp_packet_vec.model_list = vec![(RefU64::from_str("25688/19684").unwrap(), "STWALL 3".to_string())];
 //
-//     let all_hole_map = HashMap::default();
+//     let mut all_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/19686").unwrap(),
 //         name: "/1AR04TT3504K".to_string(),
@@ -209,7 +209,7 @@ async fn test_export_water_calculation_stp() -> anyhow::Result<()> {
 //     all_hole_map.insert(RefU64::from_str("25688/19684").unwrap(), map_value);
 //     stp_packet_vec.all_hole_list = vec![all_hole_map];
 //
-//     let selected_hole_map = HashMap::default();
+//     let mut selected_hole_map = HashMap::default();
 //     let map_value = vec![FloodingHole {
 //         refno: RefU64::from_str("25688/19702").unwrap(),
 //         name: "FITT 18".to_string(),
