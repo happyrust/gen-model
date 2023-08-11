4c587ad51b2cc6b54c14ed5e8e75699398be2e35
dongpengcheng <happydpc@sina.com>
2023-08-11 17:04:25 +0800

    add missing

--- src/test/test_api/test_water_calculation_stp.rs
+++ src/test/test_api/test_water_calculation_stp.rs
@@ -542,7 +542,7 @@ async fn test_export_water_calculation_stp_4() -> anyhow::Result<()> {
     dbg!(&stp_packet);
     let mgr = get_test_ams_db_manager_async().await;
     //测试将数据保存至图数据库
-    // save_stp_data_to_arangodb(&mgr, stp_packet_vec.clone()).await;
+    save_stp_data_to_arangodb(&mgr, stp_packet.clone()).await;
     //孔洞封堵
     export_stp(&mgr, stp_packet).await?;
     Ok(())
