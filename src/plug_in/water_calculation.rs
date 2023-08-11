b68ae3f0bdd2eb15c67001fbc3482ca85c47ae92
dpc <happydpc@sina.com>
2023-08-11 10:03:32 +0800

    增加贯穿件测试案例

--- src/plug_in/water_calculation.rs
+++ src/plug_in/water_calculation.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 use crate::api::children::travel_children_with_type;
 use crate::data_interface::interface::PdmsDataInterface;
-use aios_core::water_calculation::{ FloodingHole, FloodingHoleVec};
+use aios_core::water_calculation::{FloodingHole, FloodingHoleVec};
 use aios_core::water_calculation::ExportFloodingStpEvent;
 #[cfg(feature = "opencascade_rs")]
 use opencascade::primitives::Compound;
@@ -26,7 +26,7 @@ pub async fn save_stp_data_to_arangodb(aios_mgr: &AiosDBManager, mut stp: Export
         let key = hasher.finish();
         let json_data = vec![stp.to_arango_struct()];
         let Ok(send_value) = serde_json::to_value(&json_data) else { return "数据结构反序列化失败".to_string(); };
-        if let Ok(_result) = query_water_calculation_data(&database, key.to_string()).await {
+        if let Ok(_result) = query_water_calculation_data(&database, &key.to_string()).await {
             let _ = save_arangodb_doc(send_value, AQL_WATER_CALCULATION_COLLECTION, &database, true).await.unwrap();
         } else {
             let _ = save_arangodb_doc(send_value, AQL_WATER_CALCULATION_COLLECTION, &database, false).await.unwrap();
@@ -77,10 +77,9 @@ pub async fn export_stp(mgr: &AiosDBManager, stp_packet: ExportFloodingStpEvent)
 }
 
 ///查询数据库中是否已有当前名称的文件
-pub async fn query_water_calculation_data(database: &ArDatabase, key_value: String) -> anyhow::Result<Option<Vec<FloodingStpToArangodb>>> {
-    let aql = AqlQuery::new("let v = document('water_calculaion',@_key)\
-        return unset(v , '_id','_rev') ")
-        .bind_var("_key", key_value);
+pub async fn query_water_calculation_data(database: &ArDatabase, key_value: &str) -> anyhow::Result<Option<Vec<FloodingStpToArangodb>>> {
+    let aql = AqlQuery::new("let v = document('water_calculation',@_key)\
+        return unset(v , '_id','_rev') ").bind_var("_key", key_value);
     let data_vec: Vec<FloodingStpToArangodb> = database.aql_query(aql).await?;
     return Ok(Some((data_vec)));
 }
