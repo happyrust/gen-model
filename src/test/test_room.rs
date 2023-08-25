fcea50f96000b8b7e347e634e5b2df3d9b055c2d
dpc <golinuxlove@gmail.com>
2023-08-25 15:39:47 +0800

    update

--- src/test/test_room.rs
+++ src/test/test_room.rs
@@ -29,13 +29,13 @@ async fn test_query_refnos_has_neg_geom() -> anyhow::Result<()> {
 async fn test_query_through_element_rooms_1() -> anyhow::Result<()> {
     //测试样例1
     let mgr = get_test_ams_db_manager_async().await;
-    let target_refno = "24383/83512".into();
-    let r = mgr.query_eles_keypts_and_aabb_as_whole(&[target_refno], true).await?;
-    dbg!(r.unwrap().0);
-    // let room_number_map = mgr
-    //     .query_through_element_room_nums(&[target_refno])
-    //     .await?;
-    // dbg!(room_number_map);
+    let target_refno = "24383/83476".into();
+    // let r = mgr.query_eles_keypts_and_aabb_as_whole(&[target_refno], true).await?;
+
+    let room_number_map = mgr
+        .query_through_element_room_nums(&[target_refno])
+        .await?;
+    dbg!(room_number_map);
     Ok(())
 }
 //
