68babd4cb9430e6c452ad7060ec8768ca62ee808
许亲风 <1161352794@qq.com>
2023-09-11 10:35:22 +0800

    增加创建virtual_hole collection创建

--- src/main.rs
+++ src/main.rs
@@ -309,6 +309,7 @@ async fn create_arangodb_docs(db_option: &DbOption) -> anyhow::Result<()> {
     create_arango_document(&database, AQL_WATER_CALCULATION_COLLECTION, Document).await?;
     create_arango_document(&database, AQL_HOLE_EDGE_COLLECTION, Edge).await?;
     create_arango_document(&database, AQL_EMBED_EDGE_COLLECTION, Edge).await?;
+    create_arango_document(&database, AQL_VIRTUAL_HOLE_COLLECTION, Document).await?;
     Ok(())
 }
 
@@ -373,7 +374,7 @@ fn test_log() {
 async fn test_db1_dehash() {
     let mgr = AiosDBManager::init_form_config().await.unwrap();
     let refno = RefU64::from_refno_str("24383/91850").unwrap();
-    let children = mgr.get_children_within_project(refno,"AvevaMarineSample").unwrap();
+    let children = mgr.get_children_within_project(refno, "AvevaMarineSample").unwrap();
     dbg!(&children);
     let hash = db1_hash(":STACbeam");
     dbg!(&hash);
