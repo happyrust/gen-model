7ec639047e938e06bc2113b7000628a41c7d5251
dongpengcheng <happydpc@sina.com>
2023-11-14 01:50:59 +0800

    WIP parallel insert

--- src/database.rs
+++ src/database.rs
@@ -383,7 +383,6 @@ pub async fn sync_total_async_threaded(db_option: &DbOption, project: &str) -> a
                         &children_map_clone,
                     )
                     .await?;
-                    return Ok(());
                     dbg!("开始保存属性数据");
                     const ATTS_CHUNK_COUNT: usize = 500;
                     let mut join_set = tokio::task::JoinSet::new();
