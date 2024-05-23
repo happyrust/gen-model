4cf7a83ab4448e16f640f6bc18f87d187720eba4
happyrust <golinuxlove@gmail.com>
2024-05-23 12:07:58 +0800

    fix

--- src/fast_model/cata_model.rs
+++ src/fast_model/cata_model.rs
@@ -630,7 +630,8 @@ pub async fn gen_cata_geos(
         for (index, ele) in children.into_iter().enumerate() {
             let refno = ele.refno;
             let arrive_type = ele.noun.as_str();
-            let exclude = (is_hvac && leave_type != "STRT" && leave_type != "TRNS");
+            // let exclude = (is_hvac && leave_type != "STRT" && leave_type != "TRNS");
+            let exclude = false;
             {
                 // println!("正在处理直段{}: {}", cur_type, refno.to_string());
                 let world_trans = aios_core::get_world_transform(refno)
