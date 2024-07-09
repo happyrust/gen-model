6503288fdd1f445a128d9f93f895f03b8315f356
happyrust <golinuxlove@gmail.com>
2024-07-10 03:08:22 +0800

    update

--- src/cata/resolve_helper.rs
+++ src/cata/resolve_helper.rs
@@ -191,6 +191,7 @@ pub fn resolve_to_cate_geo_params(gmse: &GmseParamData) -> anyhow::Result<CateGe
             }),
             "SSLC" | "NSSL" => {
                 if gmse.paxises.len() >= 1 && gmse.diameters.len() >= 1 && gmse.shears.len() >= 4 {
+                    // dbg!(&gmse);
                     CateGeoParam::SlopeBottomCylinder(CateSlopeBottomCylinderParam {
                         refno: gmse.refno,
                         axis: (gmse.paxises[0].clone()),
